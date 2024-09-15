e6d0601afd008472c7b0c630f61c7ec95e5763f5
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-09-15 21:46:15 +0000

    return issue

--- test_main.py
+++ test_main.py
@@ -2,11 +2,11 @@ from main import get_statistics, build_barplot
 
 def test_statistics():
     result1 = get_statistics()
-    assert result1 is not None
+    assert result1 is None
 
 def test_plot():
     result2 = build_barplot()
-    assert result2 is not None
+    assert result2 is None
 
 if __name__=="__main__":
     test_statistics()
